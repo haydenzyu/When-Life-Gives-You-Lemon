fe0ed798123d73502de4f5167e67e4dee77d96b7
haydenzyu <haydenzyu@gmail.com>
2019-09-10 00:18:49 -0700

    pump arrived

--- Code/adc.py
+++ Code/adc.py
@@ -1,9 +1,19 @@
+#Hayden Yu          
+#9/1/19             
+#ADC code for soil moisture sensor
+#Followed freenove's ADC tutorial in documentation directory
+#Sensor's manual is in the datasheet directory
+
 import smbus
 import time
 
 address = 0x48 #address of PCF8591
 bus = smbus.SMBus(1)
 cmd = 0x40 #command
+wet = 120
+moist = 145
+air = 200
+water = 90
 
 def analogRead(chn): #read ADC value from chn 0,1,2, or 3
     value = bus.read_byte_data(address, cmd+chn)
@@ -13,8 +23,14 @@ def loop():
     while True:
         value = analogRead(0) #read the ADC value of channel 0
         voltage = value/255.0 *3.3 #calculate the voltage value
-        print('ADC value: %d, Voltage: %.2f' %(value, voltage))
-        time.sleep(1)
+        if value > wet and value <= moist:
+            state = 'Good'
+        elif value > moist:
+            state = 'Need Water'
+        else:
+            state = 'Too wet'
+        print('ADC value: %d, Voltage: %.2f, %s' %(value, voltage, state)) #print value to terminal
+        time.sleep(1) #reads value every minute
 
 def destroy():
     bus.close()
