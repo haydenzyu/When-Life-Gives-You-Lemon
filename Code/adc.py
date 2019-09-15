5e5f34fb380ef767d0cd40a2894f11f759f0b883
haydenzyu <haydenzyu@gmail.com>
2019-09-14 20:04:20 -0700

    Implemented self watering with relay and pump

--- Code/adc.py
+++ Code/adc.py
@@ -12,15 +12,16 @@ address = 0x48 #address of PCF8591
 bus = smbus.SMBus(1)
 cmd = 0x40 #command
 wet = 120
-moist = 145
+moist = 151
 air = 200
 water = 90
-relayPin = 40
-one_day = 1
+relayPin = 38
+one_day = 86400 #seconds in a day
 
 def setup():
     print('Program is starting...')
     GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
     GPIO.setup(relayPin, GPIO.OUT)
 
 def analogRead(chn): #read ADC value from chn 0,1,2, or 3
@@ -28,21 +29,27 @@ def analogRead(chn): #read ADC value from chn 0,1,2, or 3
     return value
 
 def loop():
+    GPIO.output(relayPin, False)
     while True:
         value = analogRead(0) #read the ADC value of channel 0
         voltage = value/255.0 *3.3 #calculate the voltage value
         if value > wet and value <= moist:
             state = 'Good'
+            GPIO.output(relayPin, False)
         elif value > moist:
             state = 'Need Water'
+            GPIO.output(relayPin, True)
+            time.sleep(5)
+            GPIO.output(relayPin, False)
+            time.sleep(1)
         else:
             state = 'Too wet'
+            GPIO.output(relayPin, False)
         print('ADC value: %d, Voltage: %.2f, %s' %(value, voltage, state)) #print value to terminal
-        time.sleep(1) #reads value every minute
-		GPIO.output(relayPin, GPIO.HIGH)
+        time.sleep(1)
 
 def destroy():
-    GPIO.output(relayPin, GPIO.LOW)
+    GPIO.output(relayPin, False)
     GPIO.cleanup()
     bus.close()
 
